cc8b915389d049194952e3b7ead4e94df0f631d9
Anton Reshetov <reshetov.art@gmail.com>
2016-06-15 08:12:38 +0300

    Clear

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,31 +1,3 @@
-/*----------------------------------*\
-/*  SLIDER
-/*----------------------------------*/
-$(function() {
-    $('.flexslider').flexslider({
-        slideshowSpeed: 3000,
-        fadeFirstSlide: false,
-        controlNav: false,
-    });
-
-    // resume play
-    $('.flex-next').click(function() {
-        $('.flexslider').flexslider("play");
-    });
-    $('.flex-prev').click(function() {
-        $('.flexslider').flexslider("play");
-    });
-    $('.flex-control-nav').click(function() {
-        $('.flexslider').flexslider("play");
-    });
-});
-
-function preCapt() {
-    $('.flex-caption').fadeIn(300);
-}
-if (window.innerWidth > 500) {
-    setTimeout(preCapt, 500);
-}
 /*----------------------------------*/
 /*  SLICK  */
 /*----------------------------------*/
@@ -37,13 +9,4 @@ $('.carousel').slick({
   nextArrow: '<div class="slick-next"></div>',
   prevArrow: '<div class="slick-prev"></div>',
 
-});
-/*----------------------------------*/
-/*  FANCYBOX  */
-/*----------------------------------*/
-$(document).ready(function() {
-  $(".fancybox").fancybox({
-    openEffect  : 'none',
-    closeEffect : 'none'
-  });
 });
