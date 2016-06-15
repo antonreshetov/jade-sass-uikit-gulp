592cf47348329d8b8e5b3f6481cc52e8aa211349
Anton Reshetov <reshetov.art@gmail.com>
2016-06-15 08:12:10 +0300

    Add Notify Error + Order uikit.js

--- gulpfile.js
+++ gulpfile.js
@@ -13,6 +13,13 @@ var concat       = require("gulp-concat");
 var order        = require("gulp-order");
 var merge        = require('merge-stream');
 var htmlpretty   = require('gulp-prettify');
+var notify       = require("gulp-notify");
+
+// Error Handler
+function swallowError (error) {
+  console.log(error.toString())
+  this.emit('end')
+}
 
 // Browser Sync
 gulp.task('browserSync', function() {
@@ -63,6 +70,11 @@ gulp.task('jade', function(){
     .pipe(jade({
       pretty: true
       }))
+    .on('error', swallowError)
+    .on('error', notify.onError({
+        message: 'Error: <%= error.message %>',
+        sound: "Basso"
+      }))
     .pipe(gulp.dest('app'))
     .pipe(browserSync.reload({stream:true}));
 });
@@ -80,6 +92,7 @@ gulp.task('jsConcat', ['jsMain'], function() {
   return gulp.src('./src/assets/js/lib/*.js')
     .pipe(order([
       "src/assets/js/lib/jquery-1.11.2.min.js",
+      "src/assets/js/lib/uikit.js",
       "src/assets/js/lib/*.js",
     ],{ base: './' }))
     .pipe(concat("bundle.js"))
@@ -95,6 +108,11 @@ gulp.task('jsMain',function(){
   return gulp.src('./src/assets/js/main.js')
     .pipe(gulp.dest('app/assets/js'))
     .pipe(uglify())
+    .on('error', swallowError)
+    .on('error', notify.onError({
+        message: 'Error: <%= error.message %>',
+        sound: "Basso"
+      }))
     .pipe(rename({
       suffix: '.min'
       }))
